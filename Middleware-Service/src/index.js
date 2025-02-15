337ea4b28a8615c4ac60db890b0c8e87ca5f5710
wbmad <bw4001@hw.ac.uk>
2025-02-15 16:25:48 +0400

    G7-10 Implement-JWT-Authentication-Middleware

--- Middleware-Service/src/index.js
+++ Middleware-Service/src/index.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/test-auth', authMiddleware.verifyToken, (req, res) => {
+  res.json({ message: 'JWT Authentication Successful', user: req.user });
+});
+
 const PORT = process.env.MIDDLEWARE_PORT || 6000;
 app.listen(PORT, () => {
   console.log(`Middleware Service running on port ${PORT}`);
---
+++ Middleware-Service/src/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+exports.verifyToken = (request, response, next) => {
+    const authHeader = request.header('Authorization');
+    if (!authHeader) {
+      return response.status(401).json({ message: 'No valid token provided: Access dinied' });
+    }
+  
+    const token = authorizationHeader.startsWith("Bearer ") 
+      ? authorizationHeader.split(" ")[1] 
+      : authorizationHeader;
+  
+    try {
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      request.user = verified;
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Token expired. Please log in again." });
+      }
+      return response.status(400).json({ message: "Invalid token provided" });
+    }
+  };
